c045d36a6c19d35c8001dbf717902ece81950254
arielarchu <arielarchu4@gmail.com>
2023-09-22 18:31:44 -0300

    style

--- tik/src/components/ViedeosPlayer/index.jsx
+++ tik/src/components/ViedeosPlayer/index.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react';
 import ReactPlayer from 'react-player/youtube';
+import './styles.module.css'
 
 function Index() {
-  
-  
 
   
   return (
     <>
-      <div>
+      <div className='video-container'>
         
-        <ReactPlayer url= "https://www.youtube.com/shorts/lEvrGkwDH0Q?feature=share" />
+        <ReactPlayer url= "https://www.youtube.com/shorts/lEvrGkwDH0Q?feature=share"  
+         width="375px"
+        height="675px" />
         
       </div>
     </>
